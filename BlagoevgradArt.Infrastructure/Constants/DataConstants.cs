be0664a873dc6a8b53af94e03c97bca1c9ccbae5
laptop_mothership <cv.to@abv.bg>
2024-04-07 17:07:37 +0300

    Added a static data constants class.

---
+++ BlagoevgradArt.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,36 @@
+﻿namespace BlagoevgradArt.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int TitleMinLength = 1;
+        public const int TitleMaxLength = 50;
+
+        public const int AuthorIdMaxLength = 42;
+        public const int GalleryIdMaxLength = 42;
+
+        public const int YearMinValue = 1900;
+
+        public const int ImagePathMaxLength = 150;
+        public const int ImageSizeMaxMb = 32;
+
+        public const int HeightCmMinValue = 1;
+        public const int HeightCmMaxLength = 255;
+
+        public const int WidthCmMinValue = 1;
+        public const int WidthCmMaxLength = 255;
+
+        public const int FirstNameMinLength = 2;
+        public const int FirstNameMaxLength = 50;
+
+        public const int LastNameMinLength = 2;
+        public const int LastNameMaxLength = 50;
+
+        public const int MaterialNameMaxLength = 35;
+        public const int ArtTypeNameMaxLength = 30;
+        public const int BaseTypeNameMaxLength = 30;
+        public const int GenreNameMaxLength = 30;
+
+        public const int GalleryNameMinLength = 3;
+        public const int GalleryNameMaxLength = 50;
+    }
+}
