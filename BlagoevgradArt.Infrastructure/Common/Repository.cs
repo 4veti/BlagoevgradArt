d01a443776e6b9f4429207701aa00eb4ffbdef37
laptop_mothership <cv.to@abv.bg>
2024-04-09 22:14:45 +0300

    Implementing a "Unit of work" pattern using the class Repository. All traffic to and from the database go through it, and using a different database would require just changing the context just in this single file.

---
+++ BlagoevgradArt.Infrastructure/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿using BlagoevgradArt.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlagoevgradArt.Infrastructure.Common
+{
+    class Repository : IRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> AllAsync<T>() where T : class
+            => DbSet<T>().AsNoTracking();
+
+        public IQueryable<T> AllAsReadOnlyAsync<T>() where T : class
+            => DbSet<T>();
+
+        public async Task AddAsync<T>(T entity) where T : class
+            => await _context.AddAsync(entity);
+
+        public async Task<int> SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+
+        private DbSet<T> DbSet<T>() where T : class
+            => _context.Set<T>();
+    }
+}
