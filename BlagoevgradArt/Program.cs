f9bdc83ec0557d26ac186740a87f2c1378ff9365
laptop_mothership <cv.to@abv.bg>
2024-04-10 23:39:05 +0300

    All changes by scaffolding Login, Logout, and Register.

--- BlagoevgradArt/Program.cs
+++ BlagoevgradArt/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BlagoevgradArt.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddApplicationDbContext(builder.Configuration);
