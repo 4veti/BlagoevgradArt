2d8fee763170bafc070a3102919ebfbb0e64c5b2
laptop_mothership <cv.to@abv.bg>
2024-04-13 00:33:23 +0300

    Implemented the painting painting controller Add methods, get and post. Added a sample form view so the app can build.

--- BlagoevgradArt.Core/Services/PaintingService.cs
+++ BlagoevgradArt.Core/Services/PaintingService.cs
@@ -1,5 +1,7 @@
 ﻿using BlagoevgradArt.Core.Contracts;
+using BlagoevgradArt.Core.Models.Painting;
 using BlagoevgradArt.Infrastructure.Data.Common;
+using BlagoevgradArt.Infrastructure.Data.Models;
 
 namespace BlagoevgradArt.Core.Services
 {
@@ -10,6 +12,31 @@ namespace BlagoevgradArt.Core.Services
         public PaintingService(IRepository repository)
         {
             _repository = repository;
+        }
+
+        public async Task<int> AddPaintingAsync(PaintingFormModel model)
+        {
+            Painting painting = new Painting()
+            {
+                Title = model.Title,
+                AuthorId = model.AuthorId,
+                ImagePath = $"Images/Paintings/{model.Title}.jpg",
+                Year = model.Year,
+                GenreId = model.GenreId,
+                ArtTypeId = model.ArtTypeId,
+                BaseTypeId = model.BaseTypeId,
+                Materials = model.Materials
+                    .Select(m => new Material() { Id = m.Id, Name = m.Name }),
+                Description = model.Description,
+                HeightCm =  model.HeightCm,
+                WidthCm = model.WidthCm,
+                IsAvailable = model.IsAvailable
+            };
+
+            await _repository.AddAsync(painting);
+            await _repository.SaveChangesAsync();
+
+            return painting.Id;
         }
     }
 }
--- BlagoevgradArt/Controllers/PaintingController.cs
+++ BlagoevgradArt/Controllers/PaintingController.cs
@@ -1,12 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlagoevgradArt.Attributes;
+using BlagoevgradArt.Core.Contracts;
+using BlagoevgradArt.Core.Models.Painting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlagoevgradArt.Controllers
 {
     public class PaintingController : BaseController
     {
+        private readonly IPaintingService _paintingService;
+        private readonly IPaintingHelperService _paintingHelperService;
+        private readonly IAuthorService _authorService;
+
+        public PaintingController(IPaintingHelperService paintingHelperService,
+            IAuthorService authorService,
+            IPaintingService paintingService)
+        {
+            _paintingService = paintingService;
+            _paintingHelperService = paintingHelperService;
+            _authorService = authorService;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+
+            return RedirectToAction(nameof(Add));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id, string modelInformation)
+        {
             return View();
+        }
+
+        [HttpGet]
+        [MustBeAuthor]
+        public async Task<IActionResult> Add()
+        {
+            PaintingFormModel model = new(await _paintingHelperService.GetGenresAsync(),
+                await _paintingHelperService.GetArtTypesAsync(),
+                await _paintingHelperService.GetBaseTypesAsync(),
+                await _paintingHelperService.GetMaterialsAsync());
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [MustBeAuthor]
+        public async Task<IActionResult> Add(PaintingFormModel model)
+        {
+            if (await _paintingHelperService.GenreExistsAsync(model.GenreId) == false)
+            {
+                ModelState.AddModelError(nameof(PaintingFormModel.GenreId), "Genre does not exist.");
+            }
+
+            if (await _paintingHelperService.ArtTypeExistsAsync(model.ArtTypeId) == false)
+            {
+                ModelState.AddModelError(nameof(PaintingFormModel.ArtTypeId), "Art type does not exist.");
+            }
+
+            if (await _paintingHelperService.BaseTypeExistsAsync(model.BaseTypeId) == false)
+            {
+                ModelState.AddModelError(nameof(PaintingFormModel.BaseTypeId), "Base type does not exist.");
+            }
+
+            if (model.Materials.Any(m => _paintingHelperService.MaterialExistsAsync(m.Id).Result == false))
+            {
+                ModelState.AddModelError(nameof(PaintingFormModel.Materials), "One or more material types do not exist.");
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return RedirectToAction(nameof(Add));                // See if this behaves as expected and keeps error messages displayed.
+            }
+
+            int id = await _paintingService.AddPaintingAsync(model);
+
+            return RedirectToAction(nameof(Details), new { id, model });
         }
     }
 }
