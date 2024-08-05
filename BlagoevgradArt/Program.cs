1867108980a80838c008ed1077440f9531540222
MothershipLaptop <ts.georgiev23@gmail.com>
2024-08-05 15:04:17 +0300

    Added anti-forgery token filter globally.

--- BlagoevgradArt/Program.cs
+++ BlagoevgradArt/Program.cs
@@ -1,10 +1,14 @@
+using BlagoevgradArt.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+});
 
 builder.Services.AddApplicationServices();
 
