f6dc0c72d5b5ca4c7477a156795d5ea13b2000e2
MothershipLaptop <ts.georgiev23@gmail.com>
2024-08-17 11:45:16 +0300

    - Added two new roles ("Author", "Gallery"). - Very minor refactoring.

--- BlagoevgradArt.Core/Constants/RoleConstants.cs
+++ BlagoevgradArt.Core/Constants/RoleConstants.cs
@@ -3,5 +3,9 @@
     public static class RoleConstants
     {
         public const string AdministratorRole = "Administrator";
+        public const string AuthorRole = "Author";
+        public const string GalleryRole = "Gallery";
+
+        public const string NoRoleText = "Няма";
     }
 }
