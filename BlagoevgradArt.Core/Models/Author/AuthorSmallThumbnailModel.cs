6239b3591cbb72837452c45d1e9dd369ffcd8c6d
MothershipLaptop <ts.georgiev23@gmail.com>
2024-09-18 14:33:06 +0300

    Added a new bool property to the AuthorSmallThumbnailModel to track if they have pending Paintings for approval. Minor refactoring.

--- BlagoevgradArt.Core/Models/Author/AuthorSmallThumbnailModel.cs
+++ BlagoevgradArt.Core/Models/Author/AuthorSmallThumbnailModel.cs
@@ -5,4 +5,6 @@ public class AuthorSmallThumbnailModel
     public int Id { get; set; }
 
     public string FullName { get; set; } = string.Empty;
+
+    public bool HasPendingPaintings { get; set; }
 }
