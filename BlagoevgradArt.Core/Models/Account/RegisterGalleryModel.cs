b264b2a16efe946b882ffcc30241d25709383ee2
MothershipLaptop <ts.georgiev23@gmail.com>
2024-12-07 20:15:31 +0200

    Refactored the refactor forms.

--- BlagoevgradArt.Core/Models/Account/RegisterGalleryModel.cs
+++ BlagoevgradArt.Core/Models/Account/RegisterGalleryModel.cs
@@ -8,6 +8,7 @@ public class RegisterGalleryModel
 {
     [Required]
     [EmailAddress]
+    [Display(Name = "Имейл")]
     public string Email { get; set; } = string.Empty;
 
     [Required]
@@ -15,10 +16,11 @@ public class RegisterGalleryModel
         MinimumLength = 6,
         ErrorMessage = InvalidLength)]
     [DataType(DataType.Password)]
+    [Display(Name = "Парола")]
     public string Password { get; set; } = string.Empty;
 
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm password")]
+    [Display(Name = "Потвърди паролата")]
     [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
     public string ConfirmPassword { get; set; } = string.Empty;
 
@@ -26,29 +28,34 @@ public class RegisterGalleryModel
     [StringLength(GalleryNameMaxLength,
             MinimumLength = GalleryNameMinLength,
             ErrorMessage = InvalidLength)]
+    [Display(Name = "Име")]
     public string Name { get; set; } = string.Empty;
 
     [Required]
     [StringLength(GalleryAddressMaxLength,
         MinimumLength = GalleryAddressMinLength,
         ErrorMessage = InvalidLength)]
+    [Display(Name = "Адрес")]
     public string Address { get; set; } = string.Empty;
 
     [Required]
     [StringLength(GalleryWorkingTimeMaxLength,
         MinimumLength = GalleryWorkingTimeMinLength,
         ErrorMessage = InvalidLength)]
+    [Display(Name = "Работно време")]
     public string WorkingTime { get; set; } = string.Empty;
 
     [Required]
     [StringLength(PhoneNumberMaxLength,
         MinimumLength = PhoneNumberMinLength,
         ErrorMessage = InvalidLength)]
+    [Display(Name = "Тел. номер")]
     public string PhoneNumber { get; set; } = string.Empty;
 
     [Required]
     [StringLength(GalleryDescriptionMaxLength,
         MinimumLength = GalleryDescriptionMinLength,
         ErrorMessage = InvalidLength)]
+    [Display(Name = "Описание")]
     public string Description { get; set; } = string.Empty;
 }
